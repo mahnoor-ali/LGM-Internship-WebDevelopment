ae28ff2768f0f8914b5105485f717c059ddc289a
Mahnoor <mahnoorali57se@gmail.com>
2022-07-26 01:02:51 +0500

    enrollment form app

---
+++ enrollmentform/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+            <nav className="navbar navbar-expand-lg bg-info mb-3 py-2">
+                <div className="container-fluid">
+                    <strong> <a className="navbar-brand ms-5 mb-3" href="/"> STUDENT ENROLLMENT FORM</a></strong>
+                </div>
+            </nav>
+    )
+}
