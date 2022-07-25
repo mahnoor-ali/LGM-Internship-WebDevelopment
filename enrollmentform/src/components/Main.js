2617d70e32c9e0761a90cded332472b0546190d6
Mahnoor <mahnoorali57se@gmail.com>
2022-07-26 03:47:50 +0500

    register students and show registration cards

--- enrollmentform/src/components/Main.js
+++ enrollmentform/src/components/Main.js
@@ -2,54 +2,62 @@ import React, { useState } from 'react'
 import Card from './Card';
 
 export default function Main() {
-    const [registrationsList, setRegistrationsList] = useState([]);
     const [formData, setFormData] = useState({ name: "", email: "", result: "", image: "", skill: "" });
+    const [registrationsList, setRegistrationsList] = useState([]);
     let screenwidth = window.screen.width;
+    const [count, setCount] = useState(0);
 
-    const enroll = event => {
-        setRegistrationsList(formData, ...registrationsList);
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setRegistrationsList([...registrationsList, formData]);
+        setCount(count+1);
+        setFormData({name: "", email: "", result: "", image: "", skill: ""});
     }
 
-    console.log("single obj: " , formData);
+    const handleInputChange = (event) => {
+       let name= event.target.name;
+       let value = event.target.value;
+        setFormData({ ...formData, [name]:value });
+    }
+
     console.log("list of obj: " , registrationsList);
     
     return (
-        <div id="main">
+        <div id="main" className='pb-5'>
             <div id="flex" style={{ width: { screenwidth } }}>
                 <div id="form">
-                    <form className="row g-3">
+                    <form className="row g-3" onSubmit={handleSubmit}>
                         <div className="col-md-12">
                             <label htmlFor="name" className="form-label">Name</label>
-                            <input value={formData.name} type="text" className="form-control" id="name" placeholder="Full name" onChange={event => setFormData({name : event.target.value})} />
+                            <input value={formData.name} type="text" className="form-control" name="name" placeholder="Full name" onChange={handleInputChange} />
                         </div>
 
                         <div className="col-12">
                             <label htmlFor="email" className="form-label">Email</label>
-                            <input value={formData.email} type="email" className="form-control" id="email" placeholder="Your email address" onChange={event => setFormData({email : event.target.value})} />
+                            <input value={formData.email} type="email" className="form-control" name="email" placeholder="Your email address" onChange={handleInputChange} />
                         </div>
                         <div className="col-12">
                             <label htmlFor="result" className="form-label">Result</label>
-                            <input value={formData.result} type="link" className="form-control" id="result" placeholder="Drive link of your result card" onChange={event => setFormData({result : event.target.value})} />
+                            <input value={formData.result} type="link" className="form-control" name="result" placeholder="Drive link of your result card" onChange={handleInputChange} />
                         </div>
                         <div className="col-md-12">
                             <label htmlFor="image" className="form-label">Image Link</label>
-                            <input value={formData.image} type="link" className="form-control" id="image" placeholder="Drive link of your image" onChange={event => setFormData({image : event.target.value})} />
+                            <input value={formData.image} type="link" className="form-control" name="image" placeholder="Drive link of your image" onChange={handleInputChange} />
                         </div>
                         <div className="col-md-12">
                             <label htmlFor="skill" className="form-label">Skills</label>
-                            <input value={formData.skill} type="text" className="form-control" id="skills" placeholder="Technical skills  eg. web development.." onChange={event => setFormData({skill : event.target.value})} />
+                            <input value={formData.skill} type="text" className="form-control" name="skill" placeholder="Technical skills  eg. web development.." onChange={handleInputChange} />
                         </div>
 
                         <div className="col-12">
-                            <button type="submit" className="btn btn-primary" onClick={enroll} >Enroll</button>
+                            <button type="submit" className="btn btn-primary" >Enroll</button>
                         </div>
                     </form>
                 </div>
                 <div id="card">
                    { registrationsList.map( (studentData) =>
-                    <Card name={studentData.name} email={studentData.email} result={studentData.result} image={studentData.image} skills={studentData.skill} />
+                    <Card key={count} name={studentData.name} email={studentData.email} result={studentData.result} image={studentData.image} skills={studentData.skill} />
                     ) }
-                    <Card/>
                 </div>
             </div>
         </div>
--- enrollmentform/src/components/Navbar.js
+++ enrollmentform/src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Navbar() {
     return (
-            <nav className="navbar navbar-expand-lg bg-info mb-3 py-2">
+            <nav className="navbar navbar-expand-lg bg-info py-3 border-bottom border-primary">
                 <div className="container-fluid">
                     <strong> <a className="navbar-brand ms-5 mb-3" href="/"> STUDENT ENROLLMENT FORM</a></strong>
                 </div>
